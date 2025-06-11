d2f0c95d4ac6bffbd30347a85fd03cb7d52c97e8
Tomáš Ondráček <t.ondracek187@gmail.com>
2025-06-11 20:59:02 +0200

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material3.android)
 
     // Hilt
     ksp(libs.hilt.compiler)
